2450f5906dd78a9fc0b2dc0d525846b6332e7071
Arlo Collins <arlocollins52@gmail.com>
2023-11-06 16:05:11 +0000

    application now works, take two

---
+++ include/App/App.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class App
+{
+public:
+
+	void Initialize();
+
+	void Update();
+	void UI();
+
+	void Release();
+
+private:
+
+};
---
+++ src/App/App.cpp
@@ -0,0 +1,18 @@
+#include "App/App.h"
+
+void App::Initialize()
+{
+}
+
+void App::Update()
+{
+}
+
+#include "imgui.h"
+void App::UI()
+{
+}
+
+void App::Release()
+{
+}
